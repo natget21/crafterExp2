fc99a4ddd2bc694d2c4d7267ef8c1afa63484f17
admin admin <evaladmin@example.com>
2024-12-06 16:24:17 +0000

    "User admin wrote content /scripts/rest/test.get.groovy"

--- scripts/rest/test.get.groovy
+++ scripts/rest/test.get.groovy
@@ -80,5 +80,13 @@
 
 
 
-def item = siteItemService.getSiteTree('/site/components/services', 2)
-return item
+// def item = siteItemService.getSiteTree('/site/components/services', 2)
+// return item
+
+
+import org.springframework.security.access.annotation.Secured
+
+@Secured(['permitAll()'])
+def handle() {
+    return ["message": "Hello, world!"]
+}
