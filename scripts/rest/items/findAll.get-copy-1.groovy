852f09057f0d91a04f676651968fe56f28b11066
admin admin <evaladmin@example.com>
2025-04-28 14:56:59 +0000

    "User admin wrote content /scripts/rest/items/findAll.get-copy-1.groovy"

--- scripts/rest/items/findAll.get-copy-1.groovy
+++ scripts/rest/items/findAll.get-copy-1.groovy
@@ -1,26 +1,3 @@
-// def searchResponse = searchClient.search(r -> r
-//     .query(q -> q
-//         .bool(b -> b
-            
-//             .must(m -> m
-//                 .wildcard(w -> w
-//                     .field("localId")
-//                     .value("/site/components/services/*")
-//                 )
-//             )
-//         )
-//     )
-// , Map)
-
-// def itemsFound = searchResponse.hits().total().value()
-// def items = searchResponse.hits().hits()*.source()
-
-// return [
-//     status: 200,
-//     itemsFound:itemsFound,
-//     items: items
-// ]
-
 
 def items = contentService.getContentStore().getObjects("/site/components/services/")
 
