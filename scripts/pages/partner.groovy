d86b9b6130558c5096b7981a4f2a49c8492bc814
vittorio carloni <vittorio.carloni@gmail.com>
2025-08-05 08:00:07 +0000

    "User vittorio wrote content /scripts/pages/partner.groovy"

--- scripts/pages/partner.groovy
+++ scripts/pages/partner.groovy
@@ -17,5 +17,5 @@ def response = client.execute(request)
 def partnerString = EntityUtils.toString(response.getEntity())
 def partnerJson = new JsonSlurper().parseText(partnerString)
 
-templateModel.test = partnerJson
+templateModel.test = response.getStatusLine().getStatusCode()
 
