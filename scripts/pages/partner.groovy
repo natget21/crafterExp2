5eeb929dd7f7809d20303e59e7909095a1d2109b
vittorio carloni <vittorio.carloni@gmail.com>
2025-08-05 10:08:39 +0000

    "User vittorio wrote content /scripts/pages/partner.groovy"

--- scripts/pages/partner.groovy
+++ scripts/pages/partner.groovy
@@ -12,7 +12,7 @@ def client = HttpClients.createDefault()
 def request = new HttpGet(url)
 request.setHeader("Accept", "application/json")
 request.setHeader("Content-Type", "application/json")
-request.setHeader("Authorization", "Bearer ${token}")
+// request.setHeader("Authorization", "Bearer ${token}")
 
 def response = client.execute(request)
 def responseString = EntityUtils.toString(response.getEntity())
