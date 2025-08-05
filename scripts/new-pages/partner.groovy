36447b192f26d3b010f430c7cef5ba0a6e51a549
vittorio carloni <vittorio.carloni@gmail.com>
2025-08-05 07:24:46 +0000

    "User vittorio wrote content /scripts/new-pages/partner.groovy"

--- scripts/new-pages/partner.groovy
+++ scripts/new-pages/partner.groovy
@@ -1,11 +1,1 @@
-import org.craftercms.core.context.CrafterContext
-import org.craftercms.engine.controller.ControllerBase
-
-class MyPageController extends ControllerBase {
-
-    @Override
-    Map handleRequest() {
-        def model = [:]
-        model.put("test", "Ciao dal controller Groovy!")
-        return model
-    }
+templateModel.test = "partnerData"
